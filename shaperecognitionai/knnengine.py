da2a4266103a74f548c8392719a7622741dfdb63
Marie-Soleil Carrier <mariecarrier@outlook.com>
2024-05-01 20:26:32 -0400

    Ajout lookup_categories pour les tags et numpyification de extract

--- shaperecognitionai/knnengine.py
+++ shaperecognitionai/knnengine.py
@@ -12,7 +12,7 @@ class KNNEngine:
         self.__processed_data = None  # vide au debut, change apres extract (grosseur *4, on veut le tag qui est le type complexe)
         self.__img_data = None  # vide au debut, property pour modif quand selection
         self.__processed_img_data = np.zeros(4)
-        self.__known_categories = {}
+        self.__known_categories = []
         self.__metrics_distance = None  # index de processed_data et la valeur de la distance
 
     @property
@@ -31,19 +31,22 @@ class KNNEngine:
     def img_data(self, data):  # ajouter le type hinting
         self.__img_data = data
 
+    def __lookup_categorie(self, tag: str) -> int:
+        if tag not in self.__known_categories:
+            self.__known_categories.append(tag)
+        return self.__known_categories.index(tag)
+
     def extract_set_data(self):
         length = len(self.__raw_data)
-        self.__processed_data = np.zeros([length, 4]) # 3 dimensions + tag, à sortir du harcodage
-
-        for i, data in enumerate(self.__raw_data): # insérer par row et par colonne en numpy?
+        self.__processed_data = np.zeros([length, 4])  # 3 dimensions + tag, à sortir du harcodage
+        for i, data in enumerate(self.__raw_data):
             metrics = FeatureExtractor.get_metrics(data[1])
-            for j, metric in enumerate(metrics):
-                self.__processed_data[i][j] = metrics[j]
+            self.__processed_data[i, :len(metrics)] = metrics
+            self.__processed_data[i, -1] = self.__lookup_categorie(data[0])
         print("METRIQUES DATASET", self.__processed_data)
-
+        print(self.__known_categories)
 
     def extract_image_data(self):
         metrics = FeatureExtractor.get_metrics(self.__img_data[1])
-        for j, metric in enumerate(metrics):
-            self.__processed_img_data[j] = metrics[j] # emeric pas content
+        self.__processed_img_data[:len(metrics)] = metrics
         print("METRIQUES IMAGE", self.__processed_img_data)
